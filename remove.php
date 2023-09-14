c729294107634c74b5f277ab9d21ac21316e6ba8
Dennis Eichhorn <spl1nes.com@googlemail.com>
2023-09-14 05:06:08 +0200

    Update remove.php

--- remove.php
+++ remove.php
@@ -1,6 +1,34 @@
 <?php
 
-delete from type_map_rel where type_map_rel_map = 'QEcIh5VFlG8Cqq1eKEE3l8Pgpzb';
-delete from map where map_uid = 'QEcIh5VFlG8Cqq1eKEE3l8Pgpzb';
-delete from finish where finish_map = '4e60b253-6dfe-4491-9516-0a88d49ec0a1';
+include __DIR__ . '/phpOMS/Autoloader.php';
+include __DIR__ . '/db.php';
+include __DIR__ . '/config.php';
+
+// load csv
+$row = 0;
+if (($handle = \fopen(__DIR__ . '/remove.csv', 'r')) !== false) {
+    while (($data = \fgetcsv($handle, 4096, ',')) !== false) {
+        ++$row;
+
+        if ($row === 1) {
+            continue;
+        }
+
+        $rel = MapTypeRelMapper::get()->where('uid', $data[0])->execute();
+        if ($rel->id !== 0) {
+            MapTypeRelMapper::delete()->execute($rel);
+        }
+
+        $rel = MapTypeRelMapper::get()->where('uid', $data[0])->execute();
+        if ($rel->id === 0) {
+            $map = MapMapper::get()->where('uid', $data[0])->execute();
+            MapMapper::delete()->execute($map);
+
+            $fins = FinishMapper::getAll()->where('map', $map->nid)->execute();
+            foreach ($fins as $fin) {
+                FinishMapper::delete()->execute($fin);
+            }
+        }
+    }
+}
 
