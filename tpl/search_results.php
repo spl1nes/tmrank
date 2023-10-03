4fbd702aa97244f98e572a9b131e9d532baa3e77
Dennis Eichhorn <spl1nes.com@googlemail.com>
2023-10-03 15:59:52 +0200

    Update search_results.php

--- tpl/search_results.php
+++ tpl/search_results.php
@@ -1,1 +1,6 @@
-
+<div id="ranking_top" class="floater">
+    <form method="GET" action="?page=user_search">
+        <input type="text" name="user_search">
+        <input type="submit" value="Search">
+    </form>
+</div>
