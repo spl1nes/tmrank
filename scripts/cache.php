4e8e2b93ddbce756838f13efabc1e13e9f88f917
Dennis Eichhorn <spl1nes.com@googlemail.com>
2023-11-08 00:25:58 +0100

    Create cache.php

---
+++ scripts/cache.php
@@ -0,0 +1,75 @@
+<?php
+
+include_once __DIR__ . '/phpOMS/Autoloader.php';
+include_once __DIR__ . '/db.php';
+
+use phpOMS\DataStorage\Database\Query\Builder;
+
+$types = MapTypeMapper::getAll()->execute();
+
+foreach (DriverMapper::yield()->execute() as $driver) {
+    foreach ($types as $type) {
+        $query = new Builder($db);
+        $query->raw(
+            'SELECT driver_uid, driver_name, score, fins, ats, golds, silvers, bronzes, ftime, rank
+            FROM (
+              SELECT
+                driver.driver_uid,
+                driver.driver_name,
+                SUM(finish.finish_finish_score) AS score,
+                COUNT(finish.finish_finish_time) AS fins,
+                COUNT(CASE WHEN finish.finish_finish_time <= map.map_at_time THEN finish.finish_finish_score ELSE NULL END) AS ats,
+                COUNT(CASE WHEN finish.finish_finish_time <= map.map_gold_time THEN finish.finish_finish_score ELSE NULL END) AS golds,
+                COUNT(CASE WHEN finish.finish_finish_time <= map.map_silver_time THEN finish.finish_finish_score ELSE NULL END) AS silvers,
+                COUNT(CASE WHEN finish.finish_finish_time <= map.map_bronze_time THEN finish.finish_finish_score ELSE NULL END) AS bronzes,
+                SUM(finish.finish_finish_time) AS ftime,
+                ROW_NUMBER() OVER (
+                    ORDER BY SUM(finish.finish_finish_score) DESC, 
+                    COUNT(finish.finish_finish_time) DESC,
+                    COUNT(CASE WHEN finish.finish_finish_time <= map.map_at_time THEN finish.finish_finish_score ELSE NULL END) DESC,
+                    COUNT(CASE WHEN finish.finish_finish_time <= map.map_gold_time THEN finish.finish_finish_score ELSE NULL END) DESC,
+                    COUNT(CASE WHEN finish.finish_finish_time <= map.map_silver_time THEN finish.finish_finish_score ELSE NULL END) DESC,
+                    COUNT(CASE WHEN finish.finish_finish_time <= map.map_bronze_time THEN finish.finish_finish_score ELSE NULL END) DESC,
+                    SUM(finish.finish_finish_time) ASC
+                ) AS rank
+              FROM driver
+              JOIN finish ON driver.driver_uid = finish.finish_driver
+              JOIN map ON finish.finish_map = map.map_nid
+              JOIN type_map_rel ON map.map_uid = type_map_rel.type_map_rel_map
+              WHERE type_map_rel.type_map_rel_type = ' . $type->id . '
+              GROUP BY driver.driver_uid, driver.driver_name
+            ) AS RankedDrivers
+            WHERE driver_uid = \'' . $uid . '\';'
+        );
+        $stats = $query->execute()->fetchAll();
+    
+        foreach ($stats as $stat) {
+            if ($stat['fins'] < 1) {
+                continue;
+            }
+          
+            $driverStat = DriverStatMapper::get()->where('uid', $uid)->where('type', $type->id)->execute();
+
+            if ($driverStat->id === 0) {
+                $driverStat = new DriverStat();
+                $driverStat->uid = $uid;
+                $driverStat->type = $type->id;
+            }
+
+            $driverStat->score = $stat['score'];
+            $driverStat->fins = $stat['fins'];
+            $driverStat->ats = $stat['ats'];
+            $driverStat->golds = $stat['golds'];
+            $driverStat->silvers = $stat['silvers'];
+            $driverStat->bronzes = $stat['bronzes'];
+            $driverStat->ftime = $stat['ftime'];
+            $driverStat->rank = $stat['rank'];
+
+            if ($driverStat->id === 0) {
+                DriverStatMapper::create()->execute($driverStat);
+            } else {
+                DriverStatMapper::update()->execute($driverStat);
+            }
+        }
+    }
+}
