4c6309d6403816ded7d10734cfb6b2414f6f88df
Dennis Eichhorn <spl1nes.com@googlemail.com>
2023-11-08 02:22:17 +0100

    Update userinfo.php

--- tpl/userinfo.php
+++ tpl/userinfo.php
@@ -10,6 +10,7 @@ if (\preg_match('/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/'
 }
 
 $types = MapTypeMapper::getAll()->execute();
+$count = [];
 
     $result = [
         'driver_uid' => $uid,
