16ac36fe19253e9a7431a25c2c0fca1cd069e7e8
Dennis Eichhorn <spl1nes.com@googlemail.com>
2023-11-08 02:34:49 +0100

    Update cache.php

--- scripts/cache.php
+++ scripts/cache.php
@@ -13,67 +13,61 @@ $types = MapTypeMapper::getAll()->execute();
 
 foreach (DriverMapper::yield()->execute() as $driver) {
     foreach ($types as $type) {
-        $query = new Builder($db);
-        $query->raw(
-            'SELECT driver_uid, driver_name, score, fins, ats, golds, silvers, bronzes, ftime, rank
-            FROM (
-              SELECT
-                driver.driver_uid,
-                driver.driver_name,
-                SUM(finish.finish_finish_score) AS score,
-                COUNT(finish.finish_finish_time) AS fins,
-                COUNT(CASE WHEN finish.finish_finish_time <= map.map_at_time THEN finish.finish_finish_score ELSE NULL END) AS ats,
-                COUNT(CASE WHEN finish.finish_finish_time <= map.map_gold_time THEN finish.finish_finish_score ELSE NULL END) AS golds,
-                COUNT(CASE WHEN finish.finish_finish_time <= map.map_silver_time THEN finish.finish_finish_score ELSE NULL END) AS silvers,
-                COUNT(CASE WHEN finish.finish_finish_time <= map.map_bronze_time THEN finish.finish_finish_score ELSE NULL END) AS bronzes,
-                SUM(finish.finish_finish_time) AS ftime,
-                ROW_NUMBER() OVER (
-                    ORDER BY SUM(finish.finish_finish_score) DESC, 
-                    COUNT(finish.finish_finish_time) DESC,
-                    COUNT(CASE WHEN finish.finish_finish_time <= map.map_at_time THEN finish.finish_finish_score ELSE NULL END) DESC,
-                    COUNT(CASE WHEN finish.finish_finish_time <= map.map_gold_time THEN finish.finish_finish_score ELSE NULL END) DESC,
-                    COUNT(CASE WHEN finish.finish_finish_time <= map.map_silver_time THEN finish.finish_finish_score ELSE NULL END) DESC,
-                    COUNT(CASE WHEN finish.finish_finish_time <= map.map_bronze_time THEN finish.finish_finish_score ELSE NULL END) DESC,
-                    SUM(finish.finish_finish_time) ASC
-                ) AS rank
-              FROM driver
-              JOIN finish ON driver.driver_uid = finish.finish_driver
-              JOIN map ON finish.finish_map = map.map_nid
-              JOIN type_map_rel ON map.map_uid = type_map_rel.type_map_rel_map
-              WHERE type_map_rel.type_map_rel_type = ' . $type->id . '
-              GROUP BY driver.driver_uid, driver.driver_name
-            ) AS RankedDrivers
-            WHERE driver_uid = \'' . $driver->uid . '\';'
-        );
-        $stats = $query->execute()->fetchAll();
+        $stats = [];
+        $offset = 0;
+        do {
+            $query = new Builder($db);
+            $query->raw(
+                'SELECT driver.driver_uid, driver.driver_name,
+                    SUM(finish.finish_finish_score) AS score,
+                    COUNT(finish.finish_finish_time) AS fins,
+                    COUNT(CASE WHEN finish.finish_finish_time <= map.map_at_time THEN finish.finish_finish_score ELSE NULL END) AS ats,
+                    COUNT(CASE WHEN finish.finish_finish_time <= map.map_gold_time THEN finish.finish_finish_score ELSE NULL END) AS golds,
+                    COUNT(CASE WHEN finish.finish_finish_time <= map.map_silver_time THEN finish.finish_finish_score ELSE NULL END) AS silvers,
+                    COUNT(CASE WHEN finish.finish_finish_time <= map.map_bronze_time THEN finish.finish_finish_score ELSE NULL END) AS bronzes,
+                    SUM(finish.finish_finish_time) AS ftime
+                FROM driver
+                JOIN finish ON driver.driver_uid = finish.finish_driver
+                JOIN map ON finish.finish_map = map.map_nid
+                JOIN type_map_rel ON map.map_uid = type_map_rel.type_map_rel_map
+                WHERE type_map_rel.type_map_rel_type = ' . ((int) type->id) . '
+                GROUP BY driver.driver_uid
+                ORDER BY score DESC, fins DESC, ats DESC, golds DESC, silvers DESC, bronzes DESC, ftime ASC
+                LIMIT 500
+                OFFSET ' . $offset . ';'
+            );
+            $stats = $query->execute()->fetchAll();
+        
+            foreach ($stats as $stat) {
+                if ($stat['fins'] < 1) {
+                    continue;
+                }
+              
+                $driverStat = DriverStatMapper::get()->where('uid', $driver->uid)->where('type', $type->id)->execute();
     
-        foreach ($stats as $stat) {
-            if ($stat['fins'] < 1) {
-                continue;
+                if ($driverStat->id === 0) {
+                    $driverStat = new DriverStat();
+                    $driverStat->uid = $driver->uid;
+                    $driverStat->type = $type->id;
+                }
+    
+                $driverStat->score = $stat['score'];
+                $driverStat->fins = $stat['fins'];
+                $driverStat->ats = $stat['ats'];
+                $driverStat->golds = $stat['golds'];
+                $driverStat->silvers = $stat['silvers'];
+                $driverStat->bronzes = $stat['bronzes'];
+                $driverStat->ftime = $stat['ftime'];
+                $driverStat->rank = $stat['rank'];
+    
+                if ($driverStat->id === 0) {
+                    DriverStatMapper::create()->execute($driverStat);
+                } else {
+                    DriverStatMapper::update()->execute($driverStat);
+                }
             }
-          
-            $driverStat = DriverStatMapper::get()->where('uid', $driver->uid)->where('type', $type->id)->execute();
 
-            if ($driverStat->id === 0) {
-                $driverStat = new DriverStat();
-                $driverStat->uid = $driver->uid;
-                $driverStat->type = $type->id;
-            }
-
-            $driverStat->score = $stat['score'];
-            $driverStat->fins = $stat['fins'];
-            $driverStat->ats = $stat['ats'];
-            $driverStat->golds = $stat['golds'];
-            $driverStat->silvers = $stat['silvers'];
-            $driverStat->bronzes = $stat['bronzes'];
-            $driverStat->ftime = $stat['ftime'];
-            $driverStat->rank = $stat['rank'];
-
-            if ($driverStat->id === 0) {
-                DriverStatMapper::create()->execute($driverStat);
-            } else {
-                DriverStatMapper::update()->execute($driverStat);
-            }
-        }
+            $offset += 500;
+        } while (!empty($stats));
     }
 }
