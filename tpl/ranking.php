2a433bbbc940ff59d757e8f1058209c420c5fbdb
Dennis Eichhorn <spl1nes.com@googlemail.com>
2023-11-09 15:35:19 +0100

    Update ranking.php

--- tpl/ranking.php
+++ tpl/ranking.php
@@ -109,7 +109,7 @@ $scores = $query->execute()->fetchAll();
                 <td><?= $score[5]; ?></td>
                 <td><?= $score[6]; ?></td>
                 <td><?= $score[7]; ?></td>
-                <td><?= \sprintf("%02d d%02dh %02dm %02ds", $days, $hours, $minutes, $seconds); ?></td>
+                <td><?= \sprintf("%02dd %02dh %02dm %02ds", $days, $hours, $minutes, $seconds); ?></td>
             </tr>
             <?php endforeach; ?>
         </tbody>
